00b670eae76a95921db77f1e807903296c523020
Lee Leathers <leeleathers@gmail.com>
2015-03-03 10:36:59 -0800

    init

---
+++ cn-navbar-directive.js
@@ -0,0 +1,101 @@
+angular.module('cnNavbar', [])
+
+    .directive('cnNavbar', [
+      '$stateParams', 'Api', '$location', 'cnSession', '$window', '$rootScope', 'EVENTS', '$state', 'VENDOR_BASE_URL'
+      function($stateParams, Api, $location, cnSession, $window, $rootScope, EVENTS, $state, VENDOR_BASE_URL) {
+        return {
+          restrict: 'E',
+          replace: true,
+          templateUrl: VENDOR_BASE_URL + 'angular-navbar/cn-navbar.html',
+          link: function($scope, elem) {
+            $scope.logout = function() {
+              Api.logout()
+                  .success(function() {
+                    // ensure a refresh of page
+                    $window.location = '/';
+                    cnSession.destroy();
+                  })
+                  .error(function(response) {
+                    log('ERROR: ' + response);
+                  })
+            };
+
+            if($stateParams.groupId) {
+              Api.getGroup($stateParams.groupId).then(function(group) {
+                $scope.group = group;
+              });
+            }
+
+            function setupCompanies() {
+//          console.log('$scope.user.companyList:', $scope.user.companyList);
+
+              $scope.companies = [
+                $scope.user.Company
+              ].concat($scope.user.companyList);
+
+              $scope.currentCompany = cnSession.getCompany();
+            }
+
+            if($scope.user) setupCompanies();
+
+            $scope.changeCompany = function(company) {
+              cnSession.setCompany(company);
+              $scope.currentCompany = company;
+            };
+
+            $scope.$watch('group.companyId', function() {
+              if($scope.group && $scope.group.companyId && $scope.group.companyId !== $scope.currentCompany.id) {
+                $scope.viewingCompany = _.find($scope.companies, {id: $scope.group.companyId});
+
+                if(!$scope.viewingCompany) {
+                  Api.getCompany($scope.group.companyId).then(function(company) {
+                    company.readOnly = true;
+                    $scope.viewingCompany = company;
+                  });
+                }
+              }
+              else {
+                $scope.viewingCompany = null;
+              }
+            });
+
+            $scope.isCompanyDisabled = function(company) {
+              return $scope.viewingCompany
+                  && $scope.viewingCompany.id !== company.id;
+            };
+
+            $scope.search = function(q) {
+              return Api.universalSearch(q);
+            };
+
+            $scope.globalNavTo = function(dest) {
+              if(dest.campaignName) {
+                $state.go('adSets', {groupId: dest.groupId, campaignId: dest.id});
+              }
+              else {
+                $state.go('campaigns', {groupId: dest.id});
+              }
+            };
+
+            $scope.analyze = function() {
+              var url = '/quicksight/#/analytics';
+
+              if($stateParams.groupId) {
+                // @todo
+              }
+
+              if($stateParams.campaignId) {
+                // @todo
+              }
+
+              $window.location = url;
+            }
+
+            $rootScope.$on(EVENTS.loginSuccess, function() {
+              $scope.user = cnSession.getUser();
+              setupCompanies();
+            });
+          }
+        }
+      }
+    ]);
